555a0601aaa35026d0e6c7baed1e54881f38dad1
SzczesnaAg <agata.szczesna89@gmail.com>
2021-04-20 11:14:59 +0200

    #index added to skip_before_action

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,14 @@ ActiveRecord::Schema.define(version: 2021_04_19_154022) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "puzzles", force: :cascade do |t|
+    t.string "title"
+    t.integer "size"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,12 +35,6 @@ ActiveRecord::Schema.define(version: 2021_04_19_154022) do
     t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-  create_table "puzzles", force: :cascade do |t|
-    t.string "title"
-    t.integer "size"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
+
 end
