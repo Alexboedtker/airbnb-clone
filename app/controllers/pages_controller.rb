9b08b4fd5cb65737e014cb5fd6dd69ae4ccdedc4
SzczesnaAg <agata.szczesna89@gmail.com>
2021-04-21 08:40:16 +0200

    show available without logging in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home,:index] #what else we can see??
+  skip_before_action :authenticate_user!, only: [:home,:index, :show] #what else we can see??
 
   def home
+  end
+
+  def index
+  end
+
+  def show
   end
 end
