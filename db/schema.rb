a63fec56575bc07ffa4fc3944f87e19dc9d673e6
SzczesnaAg <agata.szczesna89@gmail.com>
2021-04-23 13:07:08 +0200

    show and my booking new styl,rating update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_22_161117) do
+ActiveRecord::Schema.define(version: 2021_04_23_104348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,12 +53,12 @@ ActiveRecord::Schema.define(version: 2021_04_22_161117) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
     t.integer "price"
+    t.bigint "user_id", null: false
     t.string "address"
     t.float "latitude"
     t.float "longitude"
-    t.integer "rating"
+    t.float "rating", default: 0.0, null: false
     t.index ["user_id"], name: "index_puzzles_on_user_id"
   end
 
